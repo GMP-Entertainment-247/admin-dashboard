469ab0cf3e9061a96ebad6a4a8de76c2c5e4ba15
Tibesti <tobilobaolugbemi@gmail.com>
2025-09-25 17:47:18 +0100

    rap battle main pages

---
+++ src/pages/dashboard/rap-battle/viewmore/AllContestant.tsx
@@ -0,0 +1,69 @@
+import Dropdown from "../../../../components/shared/Dropdown";
+import Tabs from "../../../../components/shared/Tabs";
+import Table from "../../../../components/Table";
+import edit from "../../../../images/svg/edit.svg";
+import { imageProp } from "../../../../utils/helpers";
+
+export default function AllContestants () {
+    return (
+        <div>
+            <h2 className="text-[24px] font-semibold mb-3">All Rap Battles</h2>
+            <div>
+                <div className="bg-white px-5 py-7 -mb-5 rounded-t-xl">
+                    <Tabs
+                        tabs={[
+                            { label: "All Entries", key: "all" },
+                            { label: "Audition", key: `audition` },
+                            { label: "Stage 1", key: "stage-1" },
+                            { label: "Stage 2", key: "stage-2" },
+                            { label: "Stage 3", key: "stage-3" },
+                            { label: "Finale", key: "finale" },
+                        ]}
+                        // useAsLink
+                    />
+                </div>
+                <Table 
+                    noTitle={true}
+                    searchPlaceHolder="Search any contestant"
+                    isLoading={false}
+                    data={[1,2,3,4,5,6]}
+                    slot={
+                        <Dropdown 
+                            triggerText="Season 1"
+                            options={[]} 
+                        />
+                    }
+                    rows={[
+                        {
+                            header: "Names",
+                            view: (item) => (
+                                <div className="flex gap-2 items-center">
+                                    <div className="w-8 h-8 rounded-full flex items-center justify-center overflow-hidden">
+                                        <img {...imageProp("")} alt="" className="w-full" />
+                                    </div>
+                                    <p>John Doe</p>
+                                </div>
+                            )
+                        },
+                        {
+                            header: "Email",
+                            view: (item) => "johndoe007@gmail.com"
+                        },
+                        {
+                            header: "Phone Number",
+                            view: (item) => "08101234567"
+                        },
+                        {
+                            header: "Video Link",
+                            view: (item) => "www.abcgejdgjkded..."
+                        },
+                        {
+                            header: "Action",
+                            view: (item) => <img src={edit} alt="edit" className="w-6 ml-4" onClick={()=>{}} />
+                        },
+                    ]}
+                />
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/dashboard/rap-battle/viewmore/AllLivestream.tsx
@@ -0,0 +1,68 @@
+import Dropdown from "../../../../components/shared/Dropdown";
+import Tabs from "../../../../components/shared/Tabs";
+import Table from "../../../../components/Table";
+import { imageProp } from "../../../../utils/helpers";
+
+export default function AllLivestreams () {
+    return (
+        <div>
+            <h2 className="text-[24px] font-semibold mb-3">All Livestreams</h2>
+            <div>
+                <div className="bg-white px-5 py-7 -mb-5 rounded-t-xl">
+                    <Tabs
+                        tabs={[
+                            { label: "All Entries", key: "all" },
+                            { label: "Audition", key: `audition` },
+                            { label: "Stage 1", key: "stage-1" },
+                            { label: "Stage 2", key: "stage-2" },
+                            { label: "Stage 3", key: "stage-3" },
+                            { label: "Finale", key: "finale" },
+                        ]}
+                        // useAsLink
+                    />
+                </div>
+                <Table 
+                    noTitle={true}
+                    searchPlaceHolder="Search any contestant"
+                    isLoading={false}
+                    data={[1,2,3,4,5,6]}
+                    slot={
+                        <Dropdown 
+                            triggerText="Season 1"
+                            options={[]} 
+                        />
+                    }
+                    rows={[
+                        {
+                            header: "Title",
+                            view: (item) => (
+                                <div className="flex gap-2 items-center">
+                                    <div className="w-8 h-8 rounded-full flex items-center justify-center overflow-hidden">
+                                        <img {...imageProp("")} alt="" className="w-full" />
+                                    </div>
+                                    <p>Grand Finale</p>
+                                </div>
+                            )
+                        },
+                        {
+                            header: "Description",
+                            view: (item) => "The GMP Season 2 Rap Battle Grand Finale would be going..."
+                        },
+                        {
+                            header: "Location",
+                            view: (item) => "Lagos, Nigeria"
+                        },
+                        {
+                            header: "Date",
+                            view: (item) => "16/04/25"
+                        },
+                        {
+                            header: "Time",
+                            view: (item) => "11:25am"
+                        },
+                    ]}
+                />
+            </div>
+        </div>
+    )
+}
