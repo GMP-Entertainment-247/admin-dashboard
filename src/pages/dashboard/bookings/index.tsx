8465a9a989f4a09100b550cf79301255e79a4e7b
teecrown <teecrown@gmail.com>
2025-12-02 12:18:30 +0100

    bookingtable

--- src/pages/dashboard/bookings/index.tsx
+++ src/pages/dashboard/bookings/index.tsx
@@ -5,21 +5,20 @@ import accepted from "../../../images/svg/accepted.svg";
 import pending from "../../../images/svg/pending.svg";
 import rejected from "../../../images/svg/rejected.svg";
 import Card from "../../../components/shared/Card";
-import { useState } from "react";
-import Calendar from "react-calendar";
-import type { CalendarProps } from "react-calendar";
-
+import useFetch from "../../../utils/hooks/useFetch";
+import { IFan } from "../../../interface/fans.interface";
+import Table from "../../../components/Table";
+import EventCalendar from "../../../components/EventCalendar";
+import Dropdown from "../../../components/shared/Dropdown";
+import { imageProp } from "../../../utils/helpers";
+import edit from "../../../images/svg/edit.svg";
+// import BookingsTable  from "../artists/tables/allBookings";
+import { useNavigate } from "react-router-dom";
+import dayjs from "dayjs";
 export default function BookingsHome() {
-  const handleDateChange = (value: Value) => {
-    setSelectedDate(value);
-  };
-  type Value = CalendarProps["value"];
+  const navigate = useNavigate();
+  const { data, loading } = useFetch<{ data: IFan[] }>("/admin/list-fans");
 
-  const [selectedDate, setSelectedDate] = useState<Value>(new Date());
-
-  // const handleDateChange: CalendarProps['onChange'] = (value) => {
-  //   setSelectedDate(value);
-  // };
   const cards = [
     { id: 1, title: "sessions", icon: mic, value: "238", iconBgColor: "red" },
     {
@@ -58,68 +57,63 @@ export default function BookingsHome() {
       iconBgColor: "yellow",
     },
   ];
-  // const details =[
-  //   {
-  //     id:1,
-  //     img: mic,
-  //     EventTitle: "GMP Rap Battle",
-  //     Organizers:"ABC Media",
-  //     GuestArtist: "Kizz Val",
-  //     Location: "Lagos, Nigeria",
-  //     Date: "12th Aug, 2023",
-  //     Time: "10:00 AM - 12:00 PM",
-  //     Action: "View Details",
-
-  //   },
-  //   {
-  //     id:2,
-  //     img: mic,
-  //     EventTitle: "FelaMusic",
-  //     Organizers:"ABC Media",
-  //     GuestArtist: "Kizz Val",
-  //     Location: "Lagos, Nigeria",
-  //     Date: "12th Aug, 2023",
-  //     Time: "10:00 AM - 12:00 PM",
-  //     Action: "View Details",
-
-  //   },
-  //   {
-  //     id:3,
-  //     img: mic,
-  //     EventTitle: "GMP Rap Battle",
-  //     Organizers:"ABC Media",
-  //     GuestArtist: "Kizz Val",
-  //     Location: "Lagos, Nigeria",
-  //     Date: "12th Aug, 2023",
-  //     Time: "10:00 AM - 12:00 PM",
-  //     Action: "View Details",
-
-  //   },
-  //   {
-  //     id:4,
-  //     img: mic,
-  //     EventTitle: "Music Fest",
-  //     Organizers:"Legit Music",
-  //     GuestArtist: "Jizzy Dust",
-  //     Location: "Lagos, Nigeria",
-  //     Date: "12th Aug, 2023",
-  //     Time: "12:00 PM",
-  //     Action: "View Details",
-
-  //   },
-  //   {
-  //     id:5,
-  //     img: mic,
-  //     EventTitle: "GMP Rap Battle",
-  //     Organizers:"ABC Media",
-  //     GuestArtist: "Kizz Val",
-  //     Location: "Lagos, Nigeria",
-  //     Date: "12th Aug, 2023",
-  //     Time: "10:00 AM - 12:00 PM",
-  //     Action: "View Details",
-
-  //   }
-  // ]
+  const details = [
+    {
+      id: 1,
+      img: mic,
+      EventTitle: "GMP Rap Battle",
+      Organizers: "ABC Media",
+      GuestArtist: "Kizz Val",
+      Location: "Lagos, Nigeria",
+      Date: "12th Aug, 2023",
+      Time: "10:00 AM - 12:00 PM",
+      Action: "View Details",
+    },
+    {
+      id: 2,
+      img: mic,
+      EventTitle: "FelaMusic",
+      Organizers: "ABC Media",
+      GuestArtist: "Kizz Val",
+      Location: "Lagos, Nigeria",
+      Date: "12th Aug, 2023",
+      Time: "10:00 AM - 12:00 PM",
+      Action: "View Details",
+    },
+    {
+      id: 3,
+      img: mic,
+      EventTitle: "GMP Rap Battle",
+      Organizers: "ABC Media",
+      GuestArtist: "Kizz Val",
+      Location: "Lagos, Nigeria",
+      Date: "12th Aug, 2023",
+      Time: "10:00 AM - 12:00 PM",
+      Action: "View Details",
+    },
+    {
+      id: 4,
+      img: mic,
+      EventTitle: "Music Fest",
+      Organizers: "Legit Music",
+      GuestArtist: "Jizzy Dust",
+      Location: "Lagos, Nigeria",
+      Date: "12th Aug, 2023",
+      Time: "12:00 PM",
+      Action: "View Details",
+    },
+    {
+      id: 5,
+      img: mic,
+      EventTitle: "GMP Rap Battle",
+      Organizers: "ABC Media",
+      GuestArtist: "Kizz Val",
+      Location: "Lagos, Nigeria",
+      Date: "12th Aug, 2023",
+      Time: "10:00 AM - 12:00 PM",
+      Action: "View Details",
+    },
+  ];
 
   return (
     <div>
@@ -138,13 +132,96 @@ export default function BookingsHome() {
           ))}
         </div>
         {/*calender */}
-        <div className="ml-5 mt-5">
-          <Calendar value={selectedDate} onChange={handleDateChange} />
+        <div className="ml-5 mt-5 h-[383px]">
+          <EventCalendar />
+          {/* <Calendar value={selectedDate} onChange={handleDateChange} /> */}
         </div>
       </div>
       <div className="">
-        {/* <table className="w-full border-collapse bg-white ">
-      <thead>
+        {/* <BookingsTable/> */}
+        <Table
+          tableTitle="Bookings"
+          searchPlaceHolder="Search event title, organizer..."
+          isLoading={loading}
+          data={data?.data ?? []}
+          slot={
+            <div className="flex gap-4 items-center">
+              <Dropdown
+                triggerText="Most Recent"
+                options={[
+                  { label: "Most Recent", value: "recent" },
+                  { label: "Newest First", value: "newest" },
+                  { label: "Oldest First", value: "oldest" },
+                  { label: "A-Z", value: "desc" },
+                  { label: "Z-A", value: "asc" },
+                ]}
+              />
+              <Dropdown
+                triggerText="This Month"
+                options={[
+                  { label: "Today", value: "today" },
+                  { label: "This Week", value: "week" },
+                  { label: "This Month", value: "month" },
+                  { label: "This Year", value: "year" },
+                ]}
+              />
+            </div>
+          }
+          rows={[
+            {
+              header: "Event Title",
+              view: (details) => (
+                <div className="flex gap-2 items-center">
+                  <div className="w-8 h-8 rounded-full flex items-center justify-center overflow-hidden">
+                    <img {...imageProp("")} alt="" className="w-full" />
+                  </div>
+                  <p>{details.name}</p>
+                </div>
+              ),
+            },
+            {
+              header: "Organizers",
+              view: (item) => <span className="lowercase">{item.name}</span>,
+            },
+            {
+              header: "Guest Artist",
+              view: (item) => <span className="lowercase">{item.email}</span>,
+            },
+            {
+              header: "Location",
+              view: (details) => <span className="lowercase">{details.phone}</span>,
+            },
+            {
+              header: "Date",
+              view: (item) => (
+                <span>{dayjs(item.created_at).format("DD MMM, YYYY")}</span>
+              ),
+            },
+            {
+              header: "Time",
+              view: (item) => (
+                <span>{dayjs(item.created_at).format("DD MMM, YYYY")}</span>
+              ),
+            },
+
+            {
+              header: "Action",
+              view: (item) => (
+                <img
+                  src={edit}
+                  alt="edit"
+                  className="w-6 ml-4"
+                  onClick={() => navigate(`/bookings/${item.id}`)}
+                />
+              ),
+            },
+          ]}
+          isPreview
+          seeMoreLink="/allBookings/bookings"
+        />
+
+        <table className="w-full border-collapse bg-white ">
+          {/* <thead>
         <tr>
           <th className="border-b-2 border-gray-200 px-4 py-2 text-left">Event Title</th>
           <th className="border-b-2 border-gray-200 px-4 py-2 text-left">Organizers</th>
@@ -154,22 +231,22 @@ export default function BookingsHome() {
           <th className="border-b-2 border-gray-200 px-4 py-2 text-left">Time</th>
           <th className="border-b-2 border-gray-200 px-4 py-2 text-left">Action</th>
         </tr>
-      </thead>
-      
-        {details.map((item)=> (
-          <tbody>
-        <tr key={item.id }></tr>
-        <tr key={item.img }></tr>
-        <tr key={item.EventTitle}></tr>
-        <tr key={item.Organizers }></tr>
-        <tr key={item.GuestArtist }></tr>
-        <tr key={item.Time }></tr>
-        <tr key={item.Date }></tr>
-        <tr key={item.Location}></tr>
-        <tr key={item.Action}></tr>
-</tbody>
-        ))}
-    </table> */}
+      </thead> */}
+
+          {details.map((item) => (
+            <tbody>
+              <tr key={item.id}></tr>
+              <tr key={item.img}></tr>
+              <tr key={item.EventTitle}></tr>
+              <tr key={item.Organizers}></tr>
+              <tr key={item.GuestArtist}></tr>
+              <tr key={item.Time}></tr>
+              <tr key={item.Date}></tr>
+              <tr key={item.Location}></tr>
+              <tr key={item.Action}></tr>
+            </tbody>
+          ))}
+        </table>
       </div>
     </div>
   );
